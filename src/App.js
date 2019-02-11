a48a93e3c9ac4eb8c30bb8c88728408a999a4728
github username <github email address>
2019-02-11 14:18:02 -0500

    post new user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import GameContainer from './GameContainer'
 import Login from './Login'
+import v4 from 'uuid'
 
 class App extends Component {
   state= {
@@ -15,15 +16,30 @@ submitLogin = (info) => {
     loggedIn: true,
     currentUser: info
   })
-
+}
+createNewUser= (newUserInfo) => {
+console.log(newUserInfo);
+fetch('http://localhost:3000/api/v1/users', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  },
+  body: JSON.stringify(newUserInfo)
+})
+.then(response => response.json())
+.then(user => this.setState({
+  loggedIn: true,
+  currentUser: user
+}))
 }
   render() {
     return (
       <div className="App">
       {this.state.loggedIn ?
-      <GameContainer currentUser={this.state.currentUser} submitLogin={this.submitLogin}/>
+      <GameContainer currentUser={this.state.currentUser} submitLogin={this.submitLogin} />
       :
-      <Login submitLogin= {this.submitLogin}/>}
+      <Login submitLogin= {this.submitLogin} createNewUser= {this.createNewUser}/>}
 
       </div>
     );
--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,9 @@ import React, { Component } from 'react';
 class Login extends Component {
   state= {
     name: '',
-    email: ''
+    email: '',
+    signIn: false,
+    createUser: false
   }
   handleName= (event) => {
     this.setState({
@@ -24,17 +26,55 @@ class Login extends Component {
       email: ''
     })
   }
+  handleNewUserName= (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+  handleNewUserEmail= (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+  handleSubmitNewUser= (event) => {
+    event.preventDefault()
+    this.props.createNewUser(this.state)
+    this.setState({
+      name: '',
+      email: ''
+    })
+  }
+  handleLogin= () => {
+    this.setState({
+      signIn: !this.state.signIn
+    })
+  }
+  handleNewUser= () => {
+    this.setState({
+      createUser: !this.state.createUser
+    })
+  }
   render() {
     return (
       <div className="Login">
-      <h1> Login </h1>
-        <form onSubmit= {this.handleSubmit}>
+        <h1> Login </h1>
+        <button onClick= {this.handleLogin}>Login</button>
+        <button onClick= {this.handleNewUser}>Create User</button>
+        {this.state.signIn && <form onSubmit= {this.handleSubmit}>
         <label> Name </label>
         <input type= 'text' onChange={this.handleName} value= {this.state.name}/>
         <label> Email </label>
         <input type= 'text' onChange={this.handleEmail} value= {this.state.email}/>
         <button> Submit </button>
-        </form>
+        </form>}
+        {this.state.createUser && <form onSubmit= {this.handleSubmitNewUser}>
+        <label> Name </label>
+        <input type= 'text' onChange={this.handleNewUserName} value= {this.state.name}/>
+        <label> Email </label>
+        <input type= 'text' onChange={this.handleNewUserEmail} value= {this.state.email}/>
+        <button> Submit </button>
+        </form>}
+
       </div>
     );
   }
